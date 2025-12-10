24e0a6a8c0f5f3e524d34f53475b5d3ae38b80d5
Hector Eduardo <Eduaardo.gv03@gmail.com>
2025-12-10 13:21:12 -0600

    Fix: actualización de build.gradle y configuración del proyecto

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.softhats"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.softhats"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
